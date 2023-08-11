4f7f4c8c945ab1ba355b39832dc4b796e848b685
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-08-10 22:34:46 -0300

    Add some js classes and exercises, with the bootstrap library study too

---
+++ javascript/js-classes/class02/class02.js
@@ -0,0 +1,86 @@
+/*
+//CONDITIONAL OPERATORS
+
+// == -> equality operator;
+// != -> difference operator;
+// > -> greater than operator;
+// < -> less than operator;
+// >= -> greater than or equal to operator;
+// <= -> less than than or equal to operator;
+
+let a = 100;
+let b = 200;
+let c = a < b;
+
+//alert(c);
+
+let d = (a == b);
+//alert(d);
+
+let e = (a != b)
+//alert(e);
+
+let f = (a >= b);
+//alert(f);
+
+let g = (a <= b)
+alert(g);
+
+
+// exercise 1
+// create variables a(=1), b(=2) and display it on the console the result of:
+// a) a >= b;
+// b) a <= b;
+// c) a != b;
+// d) a == b;
+
+let a = 1;
+let b = 2;
+let c;
+
+c = (a >= b);
+console.log(c);
+c = (a <= b);
+console.log(c);
+c = (a != b);
+console.log(c);
+c = (a == b);
+console.log(c);
+*/
+
+
+/*
+// IF AND ELSE
+
+let vel;
+vel = 220;
+
+if(vel > 0 && vel < 100){
+    alert("Walking");
+}
+else if(vel > 100){
+    alert("Running");
+}
+else if(vel == 0){
+    alert("Stopped");
+}
+
+// Exercise 2
+// Build a system to display a student's grade, containing 3 possible situations:
+// 1) grade > 60: approved;
+// 2) score < 60 && > 40: you can take the recovery test;
+// 3) grade < 40: failed.
+
+console.log("----Grade_System----");
+let grade = 90;
+
+if(grade > 60){
+    console.log("APPROVED");
+}
+else if(grade >= 40 && grade <= 60){
+    console.log("You can take the recovery test");
+}
+else if(grade < 40){
+    console.log("FAILED");
+};
+*/

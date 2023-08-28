b837214ed44f84d0e05dac9e60cbbdcf74202317
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-08-27 23:03:57 -0300

    Add some examples in class005

---
+++ python/class-005.py
@@ -0,0 +1,21 @@
+# Dictionaries
+
+"""
+Lists: sorted, mutable, and duplicate-valued collection;
+Tuples: sorted, immutable, and duplicate-valued collection;
+Dictionaries: unordered, immutable collection that does not allow duplicate values;
+"""
+
+# index     0       1       2
+list1 = ["item2", "item3", "item2"];
+tuple1 = ("item2", "item3", "item2");
+
+# key = value
+dictionary1 = {"key1" : "King", "key2" : 1994, "key3": True};
+
+dictionary2 = {
+    "name": "Bruna",
+    "age": 27,
+    "nationality": "brazilian"
+}
+print(dictionary2);

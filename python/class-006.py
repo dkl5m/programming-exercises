27c7bb930cb862cb8e50505445bd1fffd93d434a
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-09-03 21:35:53 -0300

    add more examples to class006

--- python/class-006.py
+++ python/class-006.py
@@ -1,3 +1,6 @@
+# FUNCTIONS
+
+"""
 # Imperative paradigm
 # imperare = command
 
@@ -21,3 +24,91 @@ def my_function():
 
 my_function()
 print(name)
+"""
+
+# FUNCTION RETURN
+"""
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+
+return1 = list1.pop()
+var1 = print("Hello World")
+print(list1)
+print("Pop function return: ", return1)  # return popped value
+print("Print function return: ", var1)  # return nothing
+
+
+def hello_world():
+    return "Hello World!"
+
+
+print(hello_world())
+
+
+def even_odd():
+    pass  # if you're working and want to test the code with a function not
+    # finished, use pass to make the error desapear
+    # pass is used for python not to crash your code, on the first error
+
+
+even_odd()
+
+
+def even_odd1():
+    number = 4
+    if (number % 2) == 0:
+        return "even number"
+        # In the first return, python will leave the function
+    return "odd number"
+
+
+print(even_odd1())
+"""
+
+# INFINITE LOOP = ERROR
+"""
+
+def hello_world():
+    print("Hello World")
+
+
+def execute():
+    hello_world()
+
+
+execute()
+"""
+
+# FUNCTION PARAMETERS
+
+
+def my_function():
+    var = "Maria"
+    return var
+
+
+print(my_function())
+var = "Ana"
+var1 = my_function()
+print(var)
+print(var1)
+
+
+def function_name(parameter):  # parameter is the name given to the
+    # values used in the definition of the function
+    # body of function
+    print("Hello", parameter)
+
+
+name = input("What is your name?")
+function_name(name)  # argument is the name given to the values
+# used in the function call
+
+
+def print_name(name1, surname1):
+    print(f"Hello, {name1} {surname1}")
+
+
+name1 = input("What is your name?")
+surname1 = input("What is your surname?")
+function_name(name1, surname1)

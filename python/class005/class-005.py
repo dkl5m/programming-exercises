587d09ea801f7b3b67c72060341a96871f5ece03
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-09-29 23:22:51 -0300

    Add some explanition about abstraction

--- python/class005/class-005.py
+++ python/class005/class-005.py
@@ -18,16 +18,57 @@ def Register(name, age, ppr, email):
 # Object-Oriented Paradigm
 
 """
-simulation (CS) - idea of represent the real world in the computational
-world
+Structure concepts
 
-discret-event simulation - change in time
-
-Class - CamelCase
-Object
-Constructor - Python do it in it's internals
+Class - A set of objects with the same characteristics,
+        CamelCase
+Object - Representation of the real world as a type of the data
+         of the class
+Constructor - Function created implicitly with the same name as the class,
+              Python do it in it's internals
+Attribute - Variables of a class
 """
 
 
 class Patient:
-    pass
+        # __init__ access the constructor
+    def __init__(self, name, age, ppr, email):
+            # self = call the actual class
+        print("Constructor accessed")
+            # call the class attributes
+        self.name = name
+        self.age = age
+        self.ppr = ppr
+        self.email = email
+
+"""
+simulation (CS) - idea of represent the real world in the computational
+world
+
+Discret-event simulation (change in time) -> OOP
+
+Relationship -> highlighting functions(methods) and variables(attributes)
+
+-----------------------------------------------------------
+Class:
+
+Class is a structure that abstracts a set of objects with similar
+characteristics. Defining the behavior of your objects through structures:
+1 - Attributes
+2 - Methods
+
+The class defines an abstract data type
+"""
+
+# abstraction
+# reuse and cohesion
+# coupling, heritage, polymorphism, semantic gap
+"""
+Fundamental concepts
+
+Abstraction
+Process by which attributes of an object are isolated, considering what certain groups of objects have in common.
+
+Reuse
+There is no worse practice in programming than repeating code
+"""

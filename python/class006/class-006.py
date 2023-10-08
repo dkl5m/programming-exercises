2046e1549f6d9105f3f8f556973bd6dc5da9daeb
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-08 20:26:51 -0300

    Add archive with explanations for me remember of the meaning of each term

---
+++ python/class006/class-006.py
@@ -0,0 +1,51 @@
+"""
+SENDING EMAILS THEORY
+
+devices - hosts
+all computers that are connected to a
+network and participate directly in communication in
+network are classified as hosts. Hosts can
+be called endpoints
+
+server
+computers with software that allows them to provide
+information, such as email or web pages, to
+other end devices on the network
+
+each service requires server software
+separate. For example, a computer requires a
+web server software, so you can provide
+web services on the network. A computer with software
+server can simultaneously provide services to many different clients
+
+client
+Software with the ability to communicate with the
+server.
+Example of client software and a browser
+Internet. A single computer can also run
+various types of client software. For example, a
+user can check email and view
+web page while exchanging instant messages
+and listen to an audio
+
+protocols - set of rules
+In order for end devices to communicate over a network, each device
+must comply with the same set of rules. These rules have many functions
+in a network.
+
+email protocols
+email - method of storing, forwarding, sending and
+to retrieve electronic messages over a network.
+Email messages are stored in databases
+data on email servers.
+
+email client
+communicate with email servers to
+send and receive emails, through protocols.
+
+Email supports three separate protocols for
+Operation: SMTP, POP and IMAP. The process that sends
+an email uses SMTP. A customer retrieves emails
+using one of the two layer protocols
+application: POP or IMAP.
+"""

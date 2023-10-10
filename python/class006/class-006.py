880b2044a01d5535272c09a9c514340d22b0447b
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-09 23:45:04 -0300

    More explanations about network

--- python/class006/class-006.py
+++ python/class006/class-006.py
@@ -49,3 +49,100 @@ an email uses SMTP. A customer retrieves emails
 using one of the two layer protocols
 application: POP or IMAP.
 """
+
+"""
+PEER-TO-PEER
+Client and server software usually run on separate computers,
+but it is also possible for one computer to be used for both
+functions at the same time. In small businesses and homes, you
+can find computers functioning as servers and clients on the network.
+This type of network is called a peer-to-peer network.
+
+NETWORK ARCHITECTURE
+For networks to function efficiently and grow, they must be built on
+a standard network architecture.
+
+There are four basic characteristics that network architects must
+meet to meet user expectations:
+. fault tolerance
+. scalability
+. quality of service (QoS)
+. security
+
+The term network architecture refers to the technologies that
+support the infrastructure and programmed services and the rules,
+or protocols, that move data across the network.
+
+PROTOCOLS
+In order for end devices to communicate over a network, each device must
+comply with the same set of rules. These rules are called protocols and
+they have many functions in a network.
+
+URL - Universal Resource Locator
+associates address with the name of the resource on the internet
+
+URI - universal resource identifier
+each identifier is unique
+URL is part of the URI
+
+HTTP
+The hypertext transfer protocol, acronym HTTP. Hypertext is
+structured text that uses logical connections (hyperlinks) between
+us containing text. HTTP is the protocol for exchanging or
+transferring hypertext. It is the basis for web data communication
+
+An HTTP session is a sequence of request-response network
+transactions. An HTTP client initiates a request by establishing a
+Transmission Control Protocol (TCP) connection to a particular port
+on a server (typically port 80).
+
+An HTTP server listening on that port waits for a client request
+message. Upon receiving the request, the server returns a status
+line, such as "HTTP/1.1 200 OK", and its own private message.
+
+HTTPS
+Hyper text transfer protocol secure - hypertext transfer protocol
+secure, is an implementation of the HTTP protocol over an additional
+layer of security using the SSL/TLS protocol
+
+TLS is especially suited to HTTP because it can provide protection
+even if only one of the communicating parties is authenticated. This
+is the case with HTTP transactions on the internet, where typically
+only the server is authenticated.
+
+HTTP METHODS
+The HTTP protocol defines nine methods (GET, HEAD, POST, PUT, DELETE,
+TRACE, OPTIONS, CONNECT and PATCH) that indicate the action to be
+performed on the specified resource
+
+The method determines what the server should do with the URL provided
+when requesting a resource. An HTTP server must implement at least the
+GET and HEAD methods. The GET and POST methods are the ones that appear
+most commonly during web development.
+
+GET
+The GET method requests a representation of the specified resource.
+Requests using GET should only retrieve data and should not have any
+other effect.
+
+POST
+Sends data to be processed (for example, form data) to the specified
+resource
+
+PUT
+Responsible for editing a resource
+
+DELETE
+Responsible for deleting a resource
+
+HTTP STATUS CODE
+HTTP response status codes indicate whether an HTTP request was
+correctly completed. The responses are grouped into five classes:
+
+1. Information responses (100-199)
+2. Successful Answers (200-299)
+3. Redirects (300-399)
+4. Customer Errors (400-499)
+5. Server Errors (500 - 599)
+
+"""

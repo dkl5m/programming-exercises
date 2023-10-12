8f77dbfe24b9bdb1a16ca05d7f840f2a3bf52d92
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-12 16:24:40 -0300

    added food mechanics and finalized the base version of the game

--- javascript/js-classes/class06/script.js
+++ javascript/js-classes/class06/script.js
@@ -11,26 +11,97 @@ window.onload = function(){
     velX = 0;
     velY = 0;
     grid = 20;
-    size = 3;
+    size = 2;
 
     // Calling game function at each 100 ms
     setInterval(game, 100)
+
+    // Controls
+    document.addEventListener(
+        "keydown",
+        (event) => {
+            if (event.defaultPrevented) {
+                return; // Do nothing if the event was already processed
+            }
+
+            switch (event.code) {
+                case "ArrowDown":
+                    velX=0;
+                    velY=1;
+                    break;
+                case "ArrowUp":
+                    velX=0;
+                    velY=-1;
+                    break;
+                case "ArrowLeft":
+                    velX=-1;
+                    velY=0;
+                    break;
+                case "ArrowRight":
+                    velX=1;
+                    velY=0;
+                    break;
+                default:
+                    return; // Quit when this doesn't handle the key event.
+            }
+        // Cancel the default action to avoid it being handled twice
+        event.preventDefault();
+        },
+        true,
+    );
 }
 
 function game(){
     // Screen config
-    context.fillstyle = "#2980B9"
-    // distance h border, distance v border, width, height
+    context.fillStyle = "#86ebff"
+    // Distance h border, distance v border, width, height
     context.fillRect(0, 0, canvas.width, canvas.height)
 
-    // Moving the snake
-    snake.push({x: positionX, y: positionY})
-    console.log(snake[0])
+    // Snake displacement
+    positionX += velX;
+    positionY += velY;
 
-    // snake config
-    context.fillstyle = "00f102"
-    for(let i = 0; i < snake.length; i++){
-        context.fillRect(snake[i].x*grid, snake[i].y*grid, grid - 1, grid -1)
+    // Mirror
+    if(positionX < 0){
+        positionX = grid;
+    }
+    if(positionX >grid){
+        positionX = 0;
+    }
+    if(positionY < 0){
+        positionY = grid;
+    }
+    if(positionY > grid){
+        positionY = 0;
     }
 
+    // Snake config
+    context.fillStyle = "#ff702d"
+    for(let i=0; i < snake.length; i++){
+        context.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1);
+        // If the snake touches its body, it returns to its original size
+        if (snake[i].x == positionX && snake[i].y == positionY){
+            size = 2;
+        }
+    }
+
+    // Snake moving
+    snake.push({x: positionX, y: positionY})
+    //console.log(snake[0])
+
+    // Old snake body erasing
+    while(snake.length > size){
+        snake.shift();
+    }
+
+    // Food config
+    context.fillStyle = "#b00b0b";
+    context.fillRect(foodX*grid, foodY*grid, grid-1, grid-1);
+
+    // Food eating
+    if (positionX == foodX && positionY == foodY){
+        size ++;
+        foodX = Math.floor(Math.random()*grid);
+        foodY = Math.floor(Math.random()*grid);
+    }
 }

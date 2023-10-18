9cffe79fe1d9de8f5679dcff0f127a818bfc4d7c
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-17 23:54:38 -0300

    Introduction to the work of the game Codi Memory

---
+++ javascript/js-classes/class08/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Loading cards
+    const cardArray = [
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'correndo',
+            img: 'images/correndo.png'
+        },
+        {
+            name: 'correndo',
+            img: 'images/correndo.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+    ]
+})

e0daf2a83d33a4d7d6bcde5da5fa05f99e751959
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-19 01:41:02 -0300

    Add flipping cards

--- javascript/js-classes/class08/script.js
+++ javascript/js-classes/class08/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
     // Loading cards
     const cardArray = [
         {
@@ -6,12 +7,27 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/ganhou.png'
         },
         {
-            name: 'ganhou',
-            img: 'images/ganhou.png'
-        },
-        {
             name: 'correndo',
             img: 'images/correndo.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'esquerda',
+            img: 'images/esquerda.png'
+        },
+        {
+            name: 'tras',
+            img: 'images/tras.png'
+        },
+        {
+            name: 'pulo',
+            img: 'images/pulo.png'
+        },{
+            name: 'ganhou',
+            img: 'images/ganhou.png'
         },
         {
             name: 'correndo',
@@ -22,18 +38,26 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/direita.png'
         },
         {
-            name: 'direita',
-            img: 'images/direita.png'
+            name: 'esquerda',
+            img: 'images/esquerda.png'
         },
         {
-            name: 'ganhou',
-            img: 'images/ganhou.png'
+            name: 'tras',
+            img: 'images/tras.png'
         },
         {
-            name: 'ganhou',
-            img: 'images/ganhou.png'
-        },
+            name: 'pulo',
+            img: 'images/pulo.png'
+        }
     ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var pairs = []
 
     // Creating game screen
     function createBoard(){
@@ -43,6 +67,17 @@ document.addEventListener('DOMContentLoaded', () => {
             card.setAttribute('data-id', i)
             card.addEventListener('click', flipCard)
             grid.appendChild(card)
+        }
+    }
+
+    // Flipping cards
+    function flipCard(){
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if(cardsChosen.length == 2){
+            setTimeout(checkForMatch, 500)
         }
     }
 

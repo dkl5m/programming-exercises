ec237bb069fa51eef4f69b4f1accb846e16303f9
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-24 23:36:49 -0300

    Add css code and the beginning of javascript code

---
+++ javascript/js-classes/class09/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // references
+    const dino = document.querySelector('.dino')
+    const grid = document.querySelector('.grid')
+    const body = document.querySelector('.body')
+    const alert = document.querySelector('.alert')
+
+    // vars
+    let jumping = false
+    let gravity = 0.9
+    let gameo = false // game over
+    let dinopy = 0 // dino position-y
+
+})

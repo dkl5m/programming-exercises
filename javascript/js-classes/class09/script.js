28413d6c1ad60cf04337b130c7c319ca6325e62b
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-25 23:45:29 -0300

    Add jump control function and going up jump function

--- javascript/js-classes/class09/script.js
+++ javascript/js-classes/class09/script.js
@@ -12,4 +12,28 @@ document.addEventListener('DOMContentLoaded', () => {
     let gameo = false // game over
     let dinopy = 0 // dino position-y
 
+    // data input
+    document.addEventListener('keyup', jumpcontrol)
+
+    // jump control
+    function jumpcontrol(e){
+        if(e.keyCode == 32){
+            if(!jumping){
+                jumping = true
+                jump()
+            }
+        }
+    }
+    
+    function jump(){
+        let count = 0
+        let timerId = setInterval(function(){
+            // Going up
+            dinopy += 30
+            count ++
+            dinopy = dinopy * gravity
+            dino.style.bottom = dinopy + 'px'
+        }, 20)
+    }
+
 })

cf418a04000d2c041f440149742a76136f2eb23e
dkl5m <141453881+dkl5m@users.noreply.github.com>
2023-10-30 22:12:20 -0300

    Add arrays and loops

--- php/devWeb2/class01/index.php
+++ php/devWeb2/class01/index.php
@@ -9,5 +9,39 @@
     define('NAME', 'KL');
     
     echo 'Changing name var to '.$name.' and the age to '.($age - 1);
-    echo 'Const name is '.NAME;
+    echo '<h2>Const name is '.NAME.'</h2>';
+    echo '<script>alert("Hello World");</scripts>';
+
+    $students = array('Jack', 'Monica', 'Gojo');
+    echo $students[1];
+    $students2 = array('student' => 'Jack', 'Monica', 'Gojo');
+    echo $students2['student'];
+
+    foreach ($students as $key => $value){
+        echo $value;
+        echo '<br/>';
+    }
+
+    foreach ($students as $key => $value){
+        echo $key;
+        echo '<br/>';
+    }
+
+    for($i = 0; $i < 10; $i++){
+        echo $i;
+        echo '<br/>';
+    }
+
+    $stu = array('One', 'Mon', 'Hour');
+    for($i = 0; $i < sizeof($stu); $i++){
+        echo $i;
+        echo '<br/>';
+    }
+
+    $i = 0;
+    while($i < 10){
+        echo 'Message'.$i;
+        echo '<br/>';
+        $i++;
+    }
 ?>
